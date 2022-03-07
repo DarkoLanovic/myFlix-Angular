bf2f4a0112d26d0c1b30fb02f7abc6140edff9bc
Darko Lanovic <lanovic.darko@gmail.com>
2022-03-07 10:54:30 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/*
+The 'HttpClientModule' is a simplified API for Angular applications 
+that makes it possible for the client app 
+to communicate with the API or server-side
+*/
 
 @NgModule({
   declarations: [
@@ -10,7 +17,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
